457826a4558b1941678392c42e38be90af56c708
ong-ar <reclusis@gmail.com>
2019-06-24 16:49:34 +0900

    Unsubscribes

---
+++ subscription.js
@@ -0,0 +1,18 @@
+const rxjs = require("rxjs");
+
+const interval = rxjs.interval;
+
+const observable1 = interval(400);
+const observable2 = interval(300);
+
+const subscription = observable1.subscribe(x => console.log("first: " + x));
+const childSubscription = observable2.subscribe(x =>
+  console.log("second: " + x)
+);
+
+subscription.add(childSubscription);
+
+setTimeout(() => {
+  // Unsubscribes BOTH subscription and childSubscription
+  subscription.unsubscribe();
+}, 1000);

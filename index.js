e991a1a9e10d29867f78174da65bff052ef1f10d
ong-ar <reclusis@gmail.com>
2019-06-24 16:16:28 +0900

    구독 해제

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const rxjs = require("rxjs");
 const Observable = rxjs.Observable;
 
 const observable = new Observable(function subscribe(subscriber) {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
-  subscriber.complete();
-  subscriber.next(4); // Is not delivered because it would violate the contract
+  // Keep track of the interval resource
+  const intervalId = setInterval(() => {
+    subscriber.next("hi");
+  }, 1000);
+
+  // Provide a way of canceling and disposing the interval resource
+  return function unsubscribe() {
+    console.log("unsubscribe");
+    clearInterval(intervalId);
+  };
 });
 
-observable.subscribe(x => console.log(x));
+const subscribe = observable.subscribe(x => console.log(x));
+
+setTimeout(() => {
+  subscribe.unsubscribe();
+}, 5000);
